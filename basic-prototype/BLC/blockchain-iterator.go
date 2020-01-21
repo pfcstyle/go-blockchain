2c27f8285624b632471ebb26a52226e5262eabcf
yawe9101 <ywang@esri.com>
2020-01-21 16:30:04 +0800

    file seprate

---
+++ basic-prototype/BLC/blockchain-iterator.go
@@ -0,0 +1,45 @@
+package BLC
+
+import (
+	"github.com/boltdb/bolt"
+	"log"
+)
+
+type BlockchainIterator struct {
+	CurrentHash []byte
+	DB          *bolt.DB
+}
+
+func (blockchainIterator *BlockchainIterator) Next() *Block {
+
+	var block *Block
+
+	err := blockchainIterator.DB.View(func(tx *bolt.Tx) error {
+
+		b := tx.Bucket([]byte(blockTableName))
+
+		if b != nil {
+			currentBlockBytes := b.Get(blockchainIterator.CurrentHash)
+			//  获取到当前迭代器里面的currentHash所对应的区块
+			block = DeserializeBlock(currentBlockBytes)
+
+			// 更新迭代器里面CurrentHash
+			blockchainIterator.CurrentHash = block.PrevBlockHash
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return block
+
+}
+
+// 迭代器
+func (blockchain *Blockchain) Iterator() *BlockchainIterator {
+
+	return &BlockchainIterator{blockchain.Tip, blockchain.DB}
+}
--- basic-prototype/BLC/blockchain.go
+++ basic-prototype/BLC/blockchain.go
@@ -19,45 +19,6 @@ type Blockchain struct {
 	DB  *bolt.DB
 }
 
-type BlockchainIterator struct {
-	CurrentHash []byte
-	DB          *bolt.DB
-}
-
-func (blockchainIterator *BlockchainIterator) Next() *Block {
-
-	var block *Block
-
-	err := blockchainIterator.DB.View(func(tx *bolt.Tx) error {
-
-		b := tx.Bucket([]byte(blockTableName))
-
-		if b != nil {
-			currentBlockBytes := b.Get(blockchainIterator.CurrentHash)
-			//  获取到当前迭代器里面的currentHash所对应的区块
-			block = DeserializeBlock(currentBlockBytes)
-
-			// 更新迭代器里面CurrentHash
-			blockchainIterator.CurrentHash = block.PrevBlockHash
-		}
-
-		return nil
-	})
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	return block
-
-}
-
-// 迭代器
-func (blockchain *Blockchain) Iterator() *BlockchainIterator {
-
-	return &BlockchainIterator{blockchain.Tip, blockchain.DB}
-}
-
 // 遍历输出所有区块的信息
 func (blc *Blockchain) Printchain() {
 
