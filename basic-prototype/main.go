1b1f69fa32c0c8b3dc397bb5da82a1838278c2a1
yawei <ywang@esri.com>
2020-01-21 10:46:43 +0800

    init

---
+++ basic-prototype/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"go-blockchain/basic-prototype/blockchain"
+)
+
+func main() {
+	block := blockchain.NewBlock("Geneis Block", 1, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0})
+	fmt.Println(block)
+}
